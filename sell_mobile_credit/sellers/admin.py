de456b0972f2dbc2b4c519076c9ec262707c7f8a
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-12 01:49:51 +0330

    Seller model registered in admin panel

--- sell_mobile_credit/sellers/admin.py
+++ sell_mobile_credit/sellers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from sellers.models import Seller
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'credit', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at', 'credit')
