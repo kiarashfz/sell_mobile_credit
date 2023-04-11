a82376ab5b176e5d94b29f0fdcfd500d9f94c035
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-11 20:20:04 +0330

    django project created

---
+++ sell_mobile_credit/config/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI sell_mobile_credit for sell_mobile_credit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sell_mobile_credit.settings')
+
+application = get_wsgi_application()
