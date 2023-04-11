f64537bb8bea30af4e55c206d9d0fb7bee4585f6
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-12 01:49:25 +0330

    Seller model wrote

--- sell_mobile_credit/sellers/models.py
+++ sell_mobile_credit/sellers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Seller(BaseModel):
+    name = models.CharField(max_length=63)
+    credit = models.PositiveIntegerField(default=0, editable=False)
+
+    def __str__(self):
+        return self.name
