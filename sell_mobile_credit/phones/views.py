e1477c1ab62d3d5e43946b2f2dfeb8256791e724
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-12 05:35:17 +0330

    sim card viewset and charge simcr edit apis wrote

--- sell_mobile_credit/phones/views.py
+++ sell_mobile_credit/phones/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.db import transaction, IntegrityError
+from rest_framework import viewsets, status
+from rest_framework.exceptions import APIException
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from phones.models import SimCard
+from phones.serializers import ChargeSimCreditSerializer, SimCardSerializer
+
+
+class ChargeSimCreditAPIView(CreateAPIView):
+    serializer_class = ChargeSimCreditSerializer
+
+    # increase sim card credit and decrease seller credit and create charge order in one transaction
+    def perform_create(self, serializer):
+        try:
+            with transaction.atomic():
+                data = serializer.validated_data
+                sim_card, seller, amount = data['sim_card'], data['seller'], data['amount']
+                seller.buy_sim_card_credit(sim_card, amount)
+                super().perform_create(serializer)
+        except IntegrityError:
+            raise APIException('Seller does not have enough credit.', code=status.HTTP_400_BAD_REQUEST)
+
+
+class SimCardViewSet(viewsets.ModelViewSet):
+    queryset = SimCard.objects.all()
+    serializer_class = SimCardSerializer
