ae512d9722f088b4aed63d83d84e17d913eec445
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-12 05:31:13 +0330

    SellerCreditOrder and useful Seller methods registered in admin panel

--- sell_mobile_credit/sellers/admin.py
+++ sell_mobile_credit/sellers/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from sellers.models import Seller
+from sellers.models import Seller, SellerCreditOrder
 
 
 @admin.register(Seller)
 class SellerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'credit', 'created_at', 'updated_at')
+    list_display = (
+        'name', 'credit', 'is_calculations_correct', 'get_calculation_error_amount', 'total_charged_credit',
+        'total_buy_amount', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
     search_fields = ('name',)
     ordering = ('-created_at',)
     readonly_fields = ('created_at', 'updated_at', 'credit')
+
+
+@admin.register(SellerCreditOrder)
+class SellerCreditOrderAdmin(admin.ModelAdmin):
+    list_display = ('seller', 'amount', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('seller__name',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
