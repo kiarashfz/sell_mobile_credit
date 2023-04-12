25938e66b3ca248383200b093aaa55b912b84448
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-12 05:35:56 +0330

    accounting and seller viewset apis wrote

--- sell_mobile_credit/sellers/views.py
+++ sell_mobile_credit/sellers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import RetrieveAPIView
 
-# Create your views here.
+from sellers.models import Seller
+from sellers.serializers import SellerSerializer, SellerAccountingSerializer
+
+
+class SellerViewSet(viewsets.ModelViewSet):
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
+
+
+class SellerAccountingAPIView(RetrieveAPIView):
+    serializer_class = SellerAccountingSerializer
+
+    def get_object(self):
+        return Seller.objects.get(id=self.kwargs['pk'])
