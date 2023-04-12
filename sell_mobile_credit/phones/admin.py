02247e723f7dd0eeb606afc75ea4dda1b283fc27
kiarashfz <kiarashfz.fazli@gmail.com>
2023-04-12 05:29:57 +0330

    SimCard & SimCreditOrder models registered in admin panel

--- sell_mobile_credit/phones/admin.py
+++ sell_mobile_credit/phones/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from phones.models import SimCard, SimCreditOrder
+
+
+@admin.register(SimCard)
+class SimCardAdmin(admin.ModelAdmin):
+    list_display = ('number', 'credit', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('number',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at', 'credit')
+
+
+@admin.register(SimCreditOrder)
+class SimCreditOrderAdmin(admin.ModelAdmin):
+    list_display = ('sim_card', 'seller', 'amount', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('sim_card__number', 'seller__name')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
